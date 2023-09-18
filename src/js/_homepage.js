82c2d0ee826965ebec3580f6e47d486db37e4a62
Sandip Kumar Shaw <sandipshaw585@gmail.com>
2023-09-18 14:52:50 +0530

    design 55

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -25,7 +25,7 @@ if(document.querySelector(".distribution-sec")){
         autoplayButtonOutput: false,
         gutter:10,
         mouseDrag:true,
-        controls: false,
+        controls: true,
         controlsText:['<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>' , '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>'],
         responsive: {
             320: {
@@ -34,15 +34,17 @@ if(document.querySelector(".distribution-sec")){
             },
             576:{
                 items:2,
+                nav:true,
             },
             768: {
               items: 4,
-              controls: true,
-              nav:false,
+            //   controls: true,
+              nav:true,
              
             },
             992:{
                 items: 5,
+                nav:true,
             },
                      
         },
@@ -111,5 +113,8 @@ if(document.querySelector(".new-releases-sec")){
 }
 
 window.addEventListener('load', () => {
-    HomepageAnimations()
+    if(window.innerWidth>=992){
+        HomepageAnimations()
+    }
+    
 })
